2d753fce1a7f6aa77a80a70f220e0bfddcbc743b
rachel1306 <60882090+rachel1306@users.noreply.github.com>
2022-01-09 16:38:13 +0530

    working commit

---
+++ app/src/main/java/com/example/notes/db.kt
@@ -0,0 +1,32 @@
+package com.gtappdevelopers.noteapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+abstract class db : RoomDatabase() {
+
+    abstract fun getNotesDao(): NotesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: db? = null
+
+        fun getDatabase(context: Context): db {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    db::class.java,
+                    "note_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
